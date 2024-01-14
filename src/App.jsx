92910aed140edbf6cc55182e0380c4ed4d6ce7cf
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-15 00:40:19 +0300

    Create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { Route, Routes } from 'react-router'
 import './App.css'
-import Navbar from './components/Navbar'
-
+import AboutPage from './routes/AboutPage'
+import NotFoundPage from './routes/NotFoundPage'
+import WorkPage from './routes/WorkPage'
+import Layout from './components/Layout'
 function App() {
 
   return (
     <>
-      <Navbar />
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<WorkPage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
     </>
   )
 }
