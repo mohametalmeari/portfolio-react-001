1eea88b715a972d3fb90f47a537887251d35e6e3
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-15 02:36:43 +0300

    Create work header section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
+import headerImg from "../assets/header-img.svg";
 
 const Work = () => {
   return (
-    <div>
-      Work
+    <div className="work">
+      <header className="work__header">
+        <h1 className="work__header-title">Visual Designer</h1>
+        <h2 className="work__header-niche">Branding | Image making</h2>
+        <p className="work__header-text">
+          This is a template Figma file, turned into code using Anima. Learn
+          more at AnimaApp.com
+        </p>
+        <img className="work__header-img" src={headerImg} alt="Home Image" />
+      </header>
     </div>
   );
 };
