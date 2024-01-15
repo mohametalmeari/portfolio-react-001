796b3c8a9933a29e6ec6f0ba8ed1db60d27c9a28
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-16 02:21:40 +0300

    Create Contact section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,20 @@
 import { NavLink } from "react-router-dom";
 
 const links = [
-  { path: '/About', text: 'About' },
-  { path: '/', text: 'Work' },
-  { path: '#contact', text: 'Contact' },
+  { path: "/About", text: "About" },
+  { path: "/", text: "Work" },
 ];
 
 const Navbar = () => (
   <ol>
     {links.map((link) => (
       <li key={link.text}>
-        <NavLink to={link.path}>
-          {link.text}
-        </NavLink>
+        <NavLink to={link.path}>{link.text}</NavLink>
       </li>
     ))}
+    <li>
+      <a href="#contact">Contact</a>
+    </li>
   </ol>
 );
 
