d2caec411558b2c2009efbf720313b693699aece
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-15 04:13:41 +0300

    Add brands images

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import headerImg from "../assets/header-img.svg";
+import googleBrandImg from "../assets/brands/google.svg";
+import beBrandImg from "../assets/brands/be.svg";
+import dribbbleBrandImg from "../assets/brands/dribbble.svg";
+import appleBrandImg from "../assets/brands/apple.svg";
+import awwwardsBrandImg from "../assets/brands/awwwards.svg";
 
 const Work = () => {
   return (
-    <div className="work">
+    <main className="work">
       <header className="work__header">
         <h1 className="work__header-title">Visual Designer</h1>
         <h2 className="work__header-niche">Branding | Image making</h2>
@@ -13,7 +18,28 @@ const Work = () => {
         </p>
         <img className="work__header-img" src={headerImg} alt="Home Image" />
       </header>
-    </div>
+      <div className="work__brands">
+        <div className="work__brands-row">
+          <img className="work__brand-img" src={googleBrandImg} alt="Google" />
+          <img className="work__brand-img" src={beBrandImg} alt="Be" />
+        </div>
+        <div className="work__brands-row">
+          <img
+            className="work__brand-img"
+            src={dribbbleBrandImg}
+            alt="dribbble"
+          />
+          <img className="work__brand-img" src={appleBrandImg} alt="Apple" />
+        </div>
+        <div className="work__brands-row">
+          <img
+            className="work__brand-img"
+            src={awwwardsBrandImg}
+            alt="awwwards"
+          />
+        </div>
+      </div>
+    </main>
   );
 };
 
